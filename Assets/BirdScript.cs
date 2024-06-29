4fef48887f70238f51a435f5dc0ddbaebb6f5634
Isbah Malik <129406085+Isbah-23@users.noreply.github.com>
2024-06-29 20:01:50 +0500

    Basic Game Done

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,16 +6,24 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D birdbody;
     public float flapStrength;
+    public LogicScript logic;
+    bool GameOver = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !GameOver)
             birdbody.velocity = Vector2.up * flapStrength;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameOver = true;
+        logic.GameOver();
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public int player_score = 0;
     public Text score;
+    public GameObject GameOverScreen;
 
     public void addScore() {
         player_score++;
         score.text = player_score.ToString();
+    }
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver() {
+        GameOverScreen.SetActive(true);
     }
 }
